511852fe13fcf9dc25f109553446c24c039f498e
kristenmills <kristen@kristen-mills.com>
2015-01-25 21:13:10 -0500

    Three week

--- assets/js/tv/stores/event.js
+++ assets/js/tv/stores/event.js
@@ -4,10 +4,12 @@ var constants = require('../constants/event');
 var EventStore = Fluxxor.createStore({
   initialize() {
     this.events = [];
+    this.three_week = [];
     this.current = 0;
 
     this.bindActions(
       constants.UPDATE_EVENTS, this.onUpdateEvents,
+      constants.UPDATE_THREE_WEEK, this.onUpdateThreeWeek,
       constants.NEXT_EVENT, this.onNextEvent
     );
   },
@@ -20,6 +22,12 @@ var EventStore = Fluxxor.createStore({
     this.emit('change');
   },
 
+  onUpdateThreeWeek(payload) {
+    this.three_week = payload.events;
+    this.sunday = payload.sunday;
+    this.emit('change');
+  },
+
   onNextEvent() {
     this.current = (this.current + 1) % this.events.length;
     this.emit('change');
@@ -28,7 +36,8 @@ var EventStore = Fluxxor.createStore({
   getState() {
     return {
       events: this.events,
-      current: this.current
+      current: this.current,
+      three_week: this.three_week
     };
   }
 });
