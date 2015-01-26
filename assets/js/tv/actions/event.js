6845accb45c447d4c85c8029879ed3aff5d519f2
kristenmills <kristen@kristen-mills.com>
2015-01-25 20:59:56 -0500

    Event actions

--- assets/js/tv/actions/event.js
+++ assets/js/tv/actions/event.js
@@ -1,9 +1,22 @@
+require('es6-promise').polyfill();
+require('whatwg-fetch');
+
 var constants = require('../constants/event')
 
 module.exports = {
   updateEvents() {
-    var events = [];
-    this.dispatch(constants.UPDATE_EVENTS, { events: events });
+    fetch('/admin/events.json?limit=12')
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response);
+      } else {
+        return Promise.reject(new Error(response.statusText));
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      this.dispatch(constants.UPDATE_EVENTS, { events: response });
+    });
   },
 
   nextEvent() {
